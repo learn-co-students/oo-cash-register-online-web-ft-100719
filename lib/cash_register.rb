5e023525f369dbefa8038c63b6635a6e7130a920
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-23 23:52:20 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
 
+class CashRegister
+
+  attr_accessor :discount, :total
+
+  def initialize(discount=nil)
+    @discount = discount
+    @total = 0
+  end
+
+  def add_item(item, price, amount=1)
+    # binding.pry
+    self.total += price * amount
+  end
+
+ 
